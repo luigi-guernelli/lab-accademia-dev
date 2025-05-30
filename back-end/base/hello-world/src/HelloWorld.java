9739c797285506f51d061c09bfa5a2986cad89d9
Luigi Guernelli <luigi.guernelli.corso@gmail.com>
2025-05-30 11:06:41 +0200

    Create HelloWorld.java
    Hello World

---
+++ back-end/base/hello-world/src/HelloWorld.java
@@ -0,0 +1,8 @@
+/** 1. Hello World
+ * il classico programma che stampa "Hello, World!" sulla console.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
