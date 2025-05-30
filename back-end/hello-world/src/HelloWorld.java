a9dc7d6956086ceffb8f3a3e88c594e3b6ff1d86
Luigi Guernelli <luigi.guernelli.corso@gmail.com>
2025-05-30 10:47:14 +0200

    Update HelloWorld.java
    Hello World

--- back-end/hello-world/src/HelloWorld.java
+++ back-end/hello-world/src/HelloWorld.java
@@ -1,3 +1,6 @@
+/** 1.HelloWorld
+* il classico programma che stampa "Hello, World" sulla console.
+*/
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
