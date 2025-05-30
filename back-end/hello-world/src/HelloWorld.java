d30be007bb613b09d5c89f27697ef1aea5c67228
Luigi Guernelli <luigi.guernelli.corso@gmail.com>
2025-05-30 11:02:45 +0200

    Delete back-end/hello-world/src directory

--- back-end/hello-world/src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-/** 1.HelloWorld
-* il classico programma che stampa "Hello, World" sulla console.
-*/
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
