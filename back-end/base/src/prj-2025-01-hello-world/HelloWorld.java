d5fa65c66931050dea8059c8416199cd664c2fbc
Luigi Guernelli <luigi.guernelli.corso@gmail.com>
2025-06-13 14:32:21 +0200

    Create HelloWorld.java

---
+++ back-end/base/src/prj-2025-01-hello-world/HelloWorld.java
@@ -0,0 +1,13 @@
+package main;
+/**
+ * Primo programma scritto in Java
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Benvenuto nel mondo della programmazione!");
+
+	}
+
+}
