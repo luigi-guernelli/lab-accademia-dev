b155953d825ae4c62621294f70080f618893a744
Luigi Guernelli <luigi.guernelli.corso@gmail.com>
2025-06-14 20:24:02 +0200

    Create Somma.java

---
+++ back-end/base/src/prj-2025-02-somma-due-numeri/Somma.java
@@ -0,0 +1,12 @@
+/**
+ * Somma di due numeri
+ */
+public class Somma {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int somma = a + b;
+        System.out.println("La somma è: " + somma);
+    }
+}
