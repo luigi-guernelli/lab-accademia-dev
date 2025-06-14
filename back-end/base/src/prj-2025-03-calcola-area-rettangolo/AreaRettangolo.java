ec2fbd43dfb4316aa19cdb695277b1c47277b973
Luigi Guernelli <luigi.guernelli.corso@gmail.com>
2025-06-14 20:28:19 +0200

    Create AreaRettangolo.java

---
+++ back-end/base/src/prj-2025-03-calcola-area-rettangolo/AreaRettangolo.java
@@ -0,0 +1,12 @@
+/**
+ * Calcolo dell'area di un rettangolo
+ */
+public class AreaRettangolo {
+
+    public static void main(String[] args) {
+        double base = 4.5;
+        double altezza = 2.3;
+        double area = base * altezza;
+        System.out.println("L'area del rettangolo è: " + area);
+    }
+}
