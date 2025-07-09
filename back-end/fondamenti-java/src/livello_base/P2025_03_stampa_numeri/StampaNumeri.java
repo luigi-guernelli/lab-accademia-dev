bc0bb1e8d26d4662e95a1cb9f13993e42e93ca22
Luigi Guernelli <luigi.guernelli.corso@gmail.com>
2025-07-09 18:44:09 +0200

    Create StampaNumeri.java
    Stampa numeri da 1 a N es.(10) a console si vedrà l'elenco da 0 a 9.

---
+++ back-end/fondamenti-java/src/livello_base/P2025_03_stampa_numeri/StampaNumeri.java
@@ -0,0 +1,16 @@
+package livello_base.P2025_03_stampa_numeri;
+/**
+ * Stampa Numeri da 1 a N
+ * Obiettivo: Dato un numero N, stampare tutti i numeri da 1 a N.
+ *
+ * Usa il ciclo for
+ * Stampa i numeri su una riga
+ */
+public class StampaNumeri {
+    public static void main (String [] args) {
+        int numero = 10; // Imposta il valore di N
+        for (int i=0; i<numero; i++) {
+            System.out.println(i);
+        }
+    }
+}
