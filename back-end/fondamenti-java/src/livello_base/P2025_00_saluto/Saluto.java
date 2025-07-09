bdae0799fa898d707d723887f5fcd1479de720a3
Luigi Guernelli <luigi.guernelli.corso@gmail.com>
2025-07-09 18:30:11 +0200

    Create Saluto.java
    Primo programma java Hello World!

---
+++ back-end/fondamenti-java/src/livello_base/P2025_00_saluto/Saluto.java
@@ -0,0 +1,9 @@
+package livello_base.P2025_00_saluto;
+/**
+ * Saluto: Hello World! Obiettivo: Stampare a video un messaggio di benvenuto
+ */
+public class Saluto {
+    public static void main(String[] args) {
+        System.out.println("Benvenuto nel primo progetto java!");
+    }
+}
